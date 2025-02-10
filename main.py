748443b4460ee06ca6bd53d4f0279b27238ecc08
Poboynia Artem <poboyniamain@gmail.com>
2025-02-10 20:24:45 +0800

    недоделанная_версия

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import string
 import random
+import tkinter as tk
+
+password_length = 10
+
+def password_generation(symbols, password_length, using_upper_case, using_numbers, using_special_characters):
+    if using_upper_case:
+        symbols += uppercase_alphabet
+
+    if using_numbers:
+        symbols += digits
+
+    if using_special_characters:
+        symbols += special_characters
+
+    password = ''
+
+    for symbol in range(password_length):
+        password += random.choice(symbols)
+
+    print(password)
 
 symbols = string.ascii_lowercase
 uppercase_alphabet = string.ascii_uppercase
 digits = string.digits
 special_characters = string.punctuation
 
-password_length = int(input('Введите длинну пароля - '))
-using_upper_case = input('Использовать верхний регистр? ')
-using_numbers = input('Использовать цифры? ')
-using_special_characters = input('Использовать специальные символы? ')
+root = tk.Tk()
+root.title("Генератор паролей")
+root.geometry("300x400")
 
-if using_upper_case == 'Да':
-    symbols += uppercase_alphabet
+using_upper_case = tk.IntVar()
+using_upper_case_сheckbutton = tk.Checkbutton(root, text="Использовать верхний регистр", variable=using_upper_case)
+using_upper_case_сheckbutton.pack()
 
-if using_numbers == 'Да':
-    symbols += digits
+using_numbers = tk.IntVar()
+using_numbers_сheckbutton = tk.Checkbutton(root, text="Использовать цифры", variable=using_numbers)
+using_numbers_сheckbutton.pack()
 
-if using_special_characters == 'Да':
-    symbols += special_characters
+using_special_characters = tk.IntVar()
+using_special_characters_сheckbutton = tk.Checkbutton(root, text="Использовать специальные символы", variable=using_special_characters)
+using_special_characters_сheckbutton.pack()
 
-password = ''
+generation = password_generation(symbols, password_length, using_upper_case, using_numbers, using_special_characters)
+generation_password_button = tk.Button(root, text = 'Сгенерировать пароль', command = generation)
+generation_password_button.pack()
 
-for symbol in range(password_length):
-    password += random.choice(symbols)
-
-print(password)
+root.mainloop()
