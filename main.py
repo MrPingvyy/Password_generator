fe4a5e8bf5206e5abc0e5560d9691865447d2ef5
Poboynia Artem <poboyniamain@gmail.com>
2025-02-10 19:28:20 +0800

    Определение_пароля

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import string
+import random
 
 lowercase_alphabet = string.ascii_lowercase
 uppercase_alphabet = string.ascii_uppercase
 digits = string.digits
-special_characters = string.punctuationgi
+special_characters = string.punctuation
+
+password_length = int(input('Введите длинну пароля - '))
+using_upper_case = input('Использовать верхний регистр? ')
+using_numbers = input('Использовать цифры? ')
+using_special_characters = input('Использовать специальные символы? ')
