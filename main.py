d7fa74ceb5ab9e4190cd87e5560b295736f97267
Poboynia Artem <poboyniamain@gmail.com>
2025-02-10 19:43:27 +0800

    Генерация_пароля

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-lowercase_alphabet = string.ascii_lowercase
+symbols = string.ascii_lowercase
 uppercase_alphabet = string.ascii_uppercase
 digits = string.digits
 special_characters = string.punctuation
@@ -10,3 +10,19 @@ password_length = int(input('Введите длинну пароля - '))
 using_upper_case = input('Использовать верхний регистр? ')
 using_numbers = input('Использовать цифры? ')
 using_special_characters = input('Использовать специальные символы? ')
+
+if using_upper_case == 'Да':
+    symbols += uppercase_alphabet
+
+if using_numbers == 'Да':
+    symbols += digits
+
+if using_special_characters == 'Да':
+    symbols += special_characters
+
+password = ''
+
+for symbol in range(password_length):
+    password += random.choice(symbols)
+
+print(password)
